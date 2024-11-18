a1d7886b51aeff0ecae12e75fc2619eee7966c07
6ocra3 <6o3kmm@mail.ru>
2024-11-18 22:46:51 +0300

    feat: apply variation

--- src/main/java/backend/academy/fractal/FractalApp.java
+++ src/main/java/backend/academy/fractal/FractalApp.java
@@ -2,6 +2,7 @@ package backend.academy.fractal;
 
 import backend.academy.fractal.structs.Point;
 import backend.academy.fractal.transformations.Transformations;
+import backend.academy.fractal.variations.Variations;
 import java.awt.Color;
 
 public class FractalApp {
@@ -11,12 +12,14 @@ public class FractalApp {
     int[][] densityMap;
     int iterations = 1_000_000;
     Transformations transformations = new Transformations();
+    Variations variations = new Variations();
 
     public FractalApp(){
         Fractal fractal = new Fractal(width, height);
         Point point = fractal.getRandomPoint();
         for(int iter = 0; iter<iterations;iter++){
             Point newPoint = transformations.applyTransformation(point);
+            newPoint = variations.applyVariation(newPoint);
 
         }
     }
--- src/main/java/backend/academy/fractal/variations/Variations.java
+++ src/main/java/backend/academy/fractal/variations/Variations.java
@@ -1,8 +1,28 @@
 package backend.academy.fractal.variations;
 
+import backend.academy.fractal.structs.Point;
 import backend.academy.fractal.variations.implementations.SphericalVariation;
 import java.util.List;
+import java.util.Random;
 
 public class Variations {
+    Random rnd = new Random();
     List<Variation> variations = List.of(new SphericalVariation());
+
+    public Variation getVariation(){
+        int t = rnd.nextInt(variations.size());
+        return variations.get(t);
+    }
+
+    public Point applyVariation(Point point){
+        Variation variation = getVariation();
+        return applyVariation(point, variation);
+    }
+
+    public Point applyVariation(Point point, Variation variation){
+        return variation.apply(point);
+    }
+
+
+
 }
