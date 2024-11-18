daf1e0ad409dd94547ff3f843ee1ab34d4b88098
6ocra3 <6o3kmm@mail.ru>
2024-11-18 08:49:09 +0300

    feat: init commit

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,11 @@
 package backend.academy;
 
+import backend.academy.flameFractalsApp.FractalApp;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        FractalApp fractalApp = new FractalApp();
     }
 }
---
+++ src/main/java/backend/academy/flameFractalsApp/FractalApp.java
@@ -0,0 +1,14 @@
+package backend.academy.flameFractalsApp;
+
+import java.awt.Color;
+
+public class FractalApp {
+    int width = 800;
+    int height = 800;
+    Color[][] colorMap;
+    int[][] densityMap;
+
+    public FractalApp(){
+        System.out.println("Start Fractal App");
+    }
+}
